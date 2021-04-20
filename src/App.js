8dedae59cc5d443c8b4f381085dcac1e6ff4d8b5
cunhaDev <dev.mateuscunha@gmail.com>
2021-04-20 17:11:06 -0400

    first commit app frontend for project fullstack

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Home from "../src/pages/Home";
+import './App.css';
+export default function App() {
+  return (
+    <Home />
+  );
+}
---
+++ src/pages/Home/index.js
@@ -0,0 +1,33 @@
+import React,{useState, useEffect} from "react";
+import "./index.css";
+import api from "../../server/api";
+export default function Home(){
+
+const [users, setUsers] = useState([]);
+
+  useEffect(() => { 
+    async function loadUsers(){
+      api.get("/listar").then(res => {
+        setUsers(res.data)
+        console.log(res.data)
+      })
+    }
+    loadUsers();
+  },  []);
+
+
+return (
+  <div className="home-container">
+    <ul>
+    {users.map(user => (
+ <li key={user.id}>  
+    <footer>            
+    <strong>Nome: {user.nome}</strong>
+    <p>E-mail: {user.email} </p>
+  </footer>
+</li>
+    ))}
+    </ul>
+  </div>
+);
+}
